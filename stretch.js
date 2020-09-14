5777f31ac7d569c854e2986589837b659cb171ab
kai-blt <blaitong@gmail.com>
2020-09-14 14:41:29 -0700

    completed stretch 2

--- stretch.js
+++ stretch.js
@@ -4,3 +4,18 @@
 
 console.log("a defined? " + (typeof a !== 'undefined'));
 console.log("b defined? " + (typeof b !== 'undefined'));
+
+
+/* STRETCH 2 */
+function incrementBySix(num) {
+    let total = 0;
+    function addSix() {
+        total = num + 6;
+    }
+    addSix();
+    return total;
+}
+
+//Adds 6 to a provided counter total
+console.log(incrementBySix(10));
+console.log(incrementBySix(21));
